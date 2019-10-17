46aac85b9346664a548af4895f4c1261d70bb630
Giorgos Drosos <56369797+gdrosos@users.noreply.github.com>
2019-10-17 21:21:32 +0300

    Added Main

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	public static void main(String[] args) {
+		int b=5,c=4,d=3,result=0;
+		int temp = c*d;
+		for (int i=0;i<10;i++) {
+			result=(result-b*i)+temp;
+			
+		}
+	System.out.println(result);
+	}
+}
